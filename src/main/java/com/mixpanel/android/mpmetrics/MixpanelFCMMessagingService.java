4366cd3d74a393c5767af605e26c76874e6a96f1
Nathan Liu <nathan@shopbonsai.ca>
2019-03-08 10:14:14 -0500

    :sparkles: Forward intents

--- src/main/java/com/mixpanel/android/mpmetrics/MixpanelFCMMessagingService.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MixpanelFCMMessagingService.java
@@ -393,12 +393,12 @@ public class MixpanelFCMMessagingService extends FirebaseMessagingService {
             notificationTitle = "A message for you";
         }
 
-        final Intent notificationIntent = buildNotificationIntent(context, uriString, campaignId, messageId, extraLogData);
+        final Intent notificationIntent = buildNotificationIntent(context, inboundIntent, uriString, campaignId, messageId, extraLogData);
 
         return new MixpanelFCMMessagingService.NotificationData(notificationIcon, largeNotificationIcon, whiteNotificationIcon, notificationTitle, message, notificationIntent, color);
     }
 
-    private Intent buildNotificationIntent(Context context, String uriString, String campaignId, String messageId, String extraLogData) {
+    private Intent buildNotificationIntent(Context context, Intent inboundIntent, String uriString, String campaignId, String messageId, String extraLogData) {
         Uri uri = null;
         if (null != uriString) {
             uri = Uri.parse(uriString);
@@ -409,6 +409,10 @@ public class MixpanelFCMMessagingService extends FirebaseMessagingService {
             ret = getDefaultIntent(context);
         } else {
             ret = new Intent(Intent.ACTION_VIEW, uri);
+        }
+
+        if (inboundIntent != null) {
+            ret.putExtras(inboundIntent);
         }
 
         if (campaignId != null) {
